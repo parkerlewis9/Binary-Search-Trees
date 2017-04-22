6f3a5cb661983f54ec57034b8916f9102da468fb
Parker Lewis <parker.lewis14@att.net>
2017-04-22 09:51:55 -0700

    complete arbitrary inserts

--- test/bst.test.js
+++ test/bst.test.js
@@ -11,6 +11,13 @@ describe("Binary Search Trees", () => {
 
         [50,25,75,15,35,65,85].map((value) => new Node(value))
                                 .forEach((node) => bst.insert(node))
+
+                        //         50
+                        //         /\
+                        //     25      75
+                        //     /\      /\
+                        // 15   35   65   85
+
     });
     
     describe("#insert", () => {
@@ -34,6 +41,25 @@ describe("Binary Search Trees", () => {
 
             expect(expectedSeventyFive.value).to.equal(75);
             expect(expectedEightyFive.value).to.equal(85);
+        })
+
+        it("should insert an arbritrary number into a tree correctly", () => {
+            let fifteen = bst.root.left.left,
+                thirtyFive = bst.root.left.right,
+                sixtyFive = bst.root.right.left,
+                eightyFive = bst.root.right.right;
+
+
+
+            bst.insert(new Node(10))
+            bst.insert(new Node(46))
+            bst.insert(new Node(55))
+            bst.insert(new Node(92))
+
+            expect(fifteen.left.value).to.equal(10)
+            expect(thirtyFive.right.value).to.equal(46)
+            expect(sixtyFive.left.value).to.equal(55)
+            expect(eightyFive.right.value).to.equal(92)
         })
     })
 })
